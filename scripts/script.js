b4e02f9f478a7387e512a9db85c8bf612ea5b5c9
DenisIsmagilov1 <denchikism789@gmail.com>
2021-03-26 02:23:27 +0300

    wip header

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+$('.header__toggle').on("click", function() {
+  $('.header__toggle').toggleClass('active');
+  $('.header__menu').toggleClass('active');
+})
+
+let openedPopup;
+
+function showPopup(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  event.target.parentElement.lastElementChild.classList.toggle("show")
+  openedPopup = event.target;
+  window.addEventListener("click", listenClickPopup)
+}
+
+function listenClickPopup (event) {
+  if (event.target != openedPopup) {
+    window.removeEventListener("click", listenClickPopup)
+    var dropdowns = document.getElementsByClassName("header__popup");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }	
+  }
+}
